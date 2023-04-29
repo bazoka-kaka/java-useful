9d73da8238ca8bdef5221b0e758afe39e009864b
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-04-29 11:57:23 +0700

    add 03. Math

---
+++ 03. Math/Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    /**
+     * Math methods:
+     * 1. Math.max()
+     * 2. Math.min()
+     * 3. Math.sqrt()
+     * 4. Math.abs()
+     * 5. Math.random()
+     */
+    
+     // getting max value
+     System.out.println(Math.max(2, 10));
+
+     // getting min value
+     System.out.println(Math.min(2, 10));
+
+     // getting square root of a value
+     System.out.println(Math.sqrt(256));
+
+     // getting absolute value
+     System.out.println(Math.abs(-256));
+
+     // getting random value from 50 to 100
+     System.out.println((Math.random() * (100 - 50)) + 50 + 1);
+  }  
+}
